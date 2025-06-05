3cddf862aa0fe5dfbf9f63c43fd64c42248997f4
salmaniye <fatahillah.salman@gmail.com>
2025-06-05 20:55:50 +0700

    Initial Commit

---
+++ emoji-picker/emoji_pickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_pickerApp.swift
+//  emoji-picker
+//
+//  Created by Salman Fatahillah on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct emoji_pickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ emoji-pickerTests/emoji_pickerTests.swift
@@ -0,0 +1,16 @@
+//
+//  emoji_pickerTests.swift
+//  emoji-pickerTests
+//
+//  Created by Salman Fatahillah on 05/06/2025.
+//
+
+import Testing
+
+struct emoji_pickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
